9bff64e0832b4abb54e81a2f28c423c1b752a4fb
SerhiiMolodets <mr.molodets@gmail.com>
2022-09-25 17:20:37 +0300

    test2

--- lect 19 Networking URLSession/PostsController/ViewController.swift
+++ lect 19 Networking URLSession/PostsController/ViewController.swift
@@ -79,22 +79,5 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
 }
 
 
-//dont counting commits
-//dont counting commits
-
-//dont counting commits
-
-//dont counting commits
-
-//dont counting commits
-
-//dont counting commits
-
-//dont counting commits
-
-//dont counting commits
-//dont counting commits
-
-//dont counting commits
 
 
